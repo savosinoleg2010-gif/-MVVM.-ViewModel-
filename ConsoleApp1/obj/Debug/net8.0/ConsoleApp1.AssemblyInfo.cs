5cfd46ed9bdf86ba9a3f29f129c57adef81f40ba
olegsavosin74-web <238503162+savosinoleg2010-gif@users.noreply.github.com>
2026-03-26 22:38:28 +0300

    ;..;
     lk;,

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad2e3d1ab59ea60c613025376cb5fe08a7edd7ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bc933711a3b6012b9d68a8ab78e978a0ed071c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
